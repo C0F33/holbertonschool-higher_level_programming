c09201504aa89b1b371e8de95d7b2c98a7a90c41
angel bermudez <angelo1998bg@gmail.com>
2024-06-17 20:04:08 -0400

    010t00

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
---top scores
---only
-SELECT score, name
-FROM hbtn_0c_0.second_table
-ORDER BY score DESC;
+-- Lists all records of the table second_table.
+-- Records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
