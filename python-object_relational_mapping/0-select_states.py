b59abca2941a226c82228d710d5bc5d2c2e22806
angel bermudez <angelo1998bg@gmail.com>
2024-06-20 18:16:02 -0400

    00t90

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-"""this script lists all states in the db"""
-
-from MySQLdb import MySQLdb
-import sys
-from MySQLdb import MySQLdb
+"""Lists all states"""
 
 
+import MySQLdb
 
-if __name__ == "__main__":
-	username = sys.argv[1]
-	password = sys.argv[2]
-	database_name = sys.argv[3]
+from sys import argv
 
+if __name__ == '__main__':
 
-	db = MySQLdb.connect(host="localhost", port=3306, user=user, passwd=password, db=database_name)
-	cur = db.cursor()
-	cur.execute("SELECT * FROM states")
-	data = cur.fetchall()
-	for row in rows:
-		print(row)
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
 
-	db.close()
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database_name)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
