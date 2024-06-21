d1ad096d982718cf2d9ba09c358f9f338ce096ea
angel bermudez <angelo1998bg@gmail.com>
2024-06-21 19:00:49 -0400

    09848

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-from sys import argv
-import MySQLdb
+""" Write a script that list all states with the same name starting with N """
 
-"""This script lists all states starting with 'N' from the database hbtn_0e_0_usa"""
+import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-		# Establish a connection to the database
-		db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-							 passwd=argv[2], db=argv[3], charset="utf8")
-		cursor = db.cursor()
-
-		# Execute the query with the desired filter
-		query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;"
-		cursor.execute(query)
-
-		# Fetch all records and print them
-		records = cursor.fetchall()
-		for row in records:
-			print(row)
-		cursor.close()
-		db.close()
-
+    """ establishes a connection to the database """
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    records = cursor.fetchall()
+    for row in records:
+        if row[1][0] == 'N':
+            print(row)
+    cursor.close()
+    db.close()
