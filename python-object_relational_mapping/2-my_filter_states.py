02c51b307f1a2c9ff5c0b836be76c7fd434fc21d
angel bermudez <angelo1998bg@gmail.com>
2024-06-21 18:03:53 -0400

    00t12

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-
-"""This module is documented"""
 import MySQLdb
 from sys import argv
 import sys
-
+"""this is a docstring"""
 if __name__ == "__main__":
 
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
 
     cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY id ASC".format(argv[4])
-    cursor.execute(query)
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+sid ASC".format(argv[4]))
     rows = cursor.fetchall()
     for row in rows:
         if row[1] == argv[4]:
