b5da9386a61d6de01744ac078305ef39ce3e136e
angel bermudez <angelo1998bg@gmail.com>
2024-06-21 23:05:36 -0400

    12tehee

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 ''' Script t hat changes the name of a state. '''
 
-import MySQLdb
 from sys import argv
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
