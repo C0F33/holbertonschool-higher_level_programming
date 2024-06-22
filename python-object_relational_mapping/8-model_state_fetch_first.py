c43d0a00b87b1d5576dd03bed0aee196c7545041
angel bermudez <angelo1998bg@gmail.com>
2024-06-21 22:13:01 -0400

    grammar issue

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    first_state = session.query(State).order_by(State.id).fist()
+    first_state = session.query(State).order_by(State.id).first()
     if first_state is not None:
         print ("{}: {}".format(first_state.id, first_state.name))
     else:
