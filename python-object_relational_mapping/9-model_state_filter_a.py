122ed0c7bdb999aa8db9651cdb693f8c692c642b
angel bermudez <angelo1998bg@gmail.com>
2024-06-21 23:06:50 -0400

    99

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Module to retrieve data from the database"""
 import sys
-from model_state import Base, State
+from model_state import State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
