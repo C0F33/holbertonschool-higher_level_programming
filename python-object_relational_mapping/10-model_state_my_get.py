5f13a728eae7b1c6041990605527939f1397e1bd
angel bermudez <angelo1998bg@gmail.com>
2024-06-21 22:42:41 -0400

    no permissions

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-''' fetches a  specific name in db .'''
+"""fetches a specific name in db"""
 
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
