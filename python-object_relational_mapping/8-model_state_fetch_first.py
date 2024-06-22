f80f15af35def5f311fe8bebf8b4e815b457ece7
angel bermudez <angelo1998bg@gmail.com>
2024-06-21 22:40:11 -0400

    9876

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
         sys.argv[1],
         sys.argv[2],
         sys.argv[3]),
-    )
+        pool_pre_ping=True)
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
