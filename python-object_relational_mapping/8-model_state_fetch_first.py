faa26977b22cb8dce6df45e827c324278d2911ee
angel bermudez <angelo1998bg@gmail.com>
2024-06-21 22:35:21 -0400

    898

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
-'''
-comment this is a string
-'''
+"""comment this is a string"""
 
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
@@ -14,7 +12,6 @@ if __name__ == "__main__":
         sys.argv[1],
         sys.argv[2],
         sys.argv[3]),
-        pool_pre_ping=True
     )
     Session = sessionmaker(bind=engine)
     session = Session()
