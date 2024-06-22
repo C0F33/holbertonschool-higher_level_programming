020dbd4b68763cb0c0ee3cdf9e4165c47dd7816d
angel bermudez <angelo1998bg@gmail.com>
2024-06-21 22:47:48 -0400

    09yty

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""filters through states in database"""
-from sqlalchemy import create_engine, text
+"""Module to retrieve data from the database"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sys import argv
-from model_state import State, Base
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/\
-                           {}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    s = '%a%'
-    states = session.query(State).filter(State.name.like(s)).order_by(State.id)
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
-    session.close()
+    try:
+        engine = create_engine( "mysql+mysqldb://{}:{}@localhost/{}".format(str(sys.argv[1]), str(sys.argv[2]), str(sys.argv[3])), pool_pre_ping=True)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        rows = session.query(State).filter(State.name.like("%a%")).all()
+        for row in rows:
+            print("{0}: {1}".format(row.id, row.name))
+    except Exception as e:
+        print("An error occurred: ", e)
