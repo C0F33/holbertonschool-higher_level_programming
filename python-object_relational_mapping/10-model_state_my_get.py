3e0436e096046a98774b44ef53169988c258f5aa
angel bermudez <angelo1998bg@gmail.com>
2024-06-21 22:54:48 -0400

    10yu

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
-"""fetches a specific name in db"""
-
+"""Nameless module to suck data out from the database
+"""
+import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sys import argv
-from model_state import State, Base
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/\
-                           {}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).filter_by(name=argv[4]).first()
-    if state is not None:
-        print(str(state.id))
-    else:
+
+    state_name = sys.argv[4]
+
+    query = session.query(State).where(State.name == state_name)
+
+    if query.count() == 0:
         print("Not found")
-    session.close()
+    else:
+        row = query.limit(1).one()
+        print("{0}".format(row.id))
