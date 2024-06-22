6aaa9990432ca11f5ac20bded40ffb096bc7bde8
angel bermudez <angelo1998bg@gmail.com>
2024-06-21 21:18:51 -0400

    12t009

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
-''' This is a docstr ing for.'''
+"""insert state in db"""
+
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 from sys import argv
 from model_state import State, Base
 
+
 if __name__ == "__main__":
+	user = argv[1]
+	passwd = argv[2]
+	host = 'localhost'
+	db = argv[3]
 
-    user = argv[1]
-    passwd = argv[2]
-    host = 'localhost'
-    db = argv[3]
+	engine = create_engine('mysql+mysqldb://\
+                        {}:{}@localhost/{}'.format(user, passwd, db))
+	session = sessionmaker(bind=engine)
+	Session = session()
 
-    engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                           .format(user, passwd, host, db))
-    session = sessionmaker(bind=engine)
-    Session = session()
+	insertState = State(name="Louisiana")
+	Session.add(insertState)
+	Session.commit()
 
-    insertState = State(name="Louisiana")
-    Session.add(insertState)
-    Session.commit()
+	print("{}".format(insertState.id))
 
-    print("{}".format(insertState.id))
+	Session.close()
 
-    Session.close()
