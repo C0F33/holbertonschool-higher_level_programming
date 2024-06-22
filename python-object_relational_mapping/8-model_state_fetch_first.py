31463339c2210e0532cb959f09a7f09c3bb69119
angel bermudez <angelo1998bg@gmail.com>
2024-06-21 22:27:09 -0400

    uuu

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     s = '%a%'
-    states = session.query(State).filter(State.name.like(s)).order_by(State.id)
+    states = session.query(State).filter(State.name.like(s)).order_by(State.id).first()
     for state in states:
         print("{}: {}".format(state.id, state.name))
     session.close()
